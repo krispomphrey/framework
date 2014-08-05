e431719afa575c480ab7a680cbd37dc978831558
Kris Pomphrey <kris@krispomphrey.co.uk>
2014-08-05 21:14:19 +0100

    Commit changes

--- Config/settings.php
+++ Config/settings.php
@@ -2,7 +2,7 @@
 class Config{
 	public $db_host = '127.0.0.1';
 	public $db_user = 'root';
-	public $db_password = 's10052';
+	public $db_password = 'supermassive';
 	public $dbs = array('fw_data');
 	public $debug = 0;
 }
