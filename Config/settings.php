fea11743805317c0c05298cc6615ac24503125b3
Kris Pomphrey <kris@krispomphrey.co.uk>
2014-08-13 22:54:50 +0100

    Added a simple logger to the framework

--- Config/settings.php
+++ Config/settings.php
@@ -9,17 +9,26 @@
  */
 class Config{
   public function __construct(){
-  	// An example of how to register a database.
-    /* $this->dbs = array(
+  	/* An example of how to register a database.
+     * For multiple databases, add to the array with 'db_name' => 'options'.  All options are needed, even if blank. */
+
+    $this->dbs = array(
       'fw_data' => array(
         'type' => 'mysqli',
-        'user' => 'DB_USER',
-        'password' => 'DB_PASSWORD',
+        'user' => 'root',
+        'password' => 'supermassive',
         'host' => 'localhost',
         'prefix' => ''
       )
-    ); */
+    );
+
+    // Whether to output debugging information or not.
   	$this->debug = true;
+
+    // Whether to expose the logger to the app.
+    $this->logging = true;
+
+    // Sets the base url of the site.  Leave as default for multi domain sites.
     $this->base_url = $_SERVER['HTTP_HOST'];
   }
 }
